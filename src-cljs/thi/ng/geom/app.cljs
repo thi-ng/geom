1df3673543eca96f18a3ef49c3a9cb7112a3ee2c
Karsten Schmidt <k@postspectacular.com>
2013-08-15 12:21:15 +0100

    updating/bugfixing physics, updating test app and adding viz.core ns

--- src-cljs/thi/ng/geom/app.cljs
+++ src-cljs/thi/ng/geom/app.cljs
@@ -1,9 +1,40 @@
 (ns thi.ng.geom.app
   (:require
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.core :as g]
+   [thi.ng.physics.core :as phys]
+   [thi.ng.viz.core :as viz]))
 
 (defn ^:export main
-  []
+  [num-frames]
   (let [points [[0 0] [100 0] [100 100] [200 100]]
-        bnd (g/bounding-rect* points)]
-    (.log js/console (clj->js (g/centroid bnd)))))
+        particles (vec (map #(phys/map->VerletParticle
+                              {:pos % :prev %
+                               :weight 1 :inv-weight 1
+                               :force [0 0]
+                               :behaviors []
+                               :constraints []})
+                            points))
+        particles (update-in particles [0] phys/lock)
+        particles (update-in particles [(dec (count points))] phys/lock)
+        springs (vec (map #(phys/map->VerletSpring
+                            {:aid % :bid (inc %) :rlen 150 :strength 0.1})
+                          (range (dec (count points)))))
+        p (phys/map->VerletPhysics
+           {:particles particles
+            :springs springs
+            :behaviors []
+            :constraints []
+            :timestep 1
+            :iter 50
+            :drag 0.95})
+        canv (viz/make-canvas :width 640 :height 480 :parent "main")
+        ctx (viz/canvas-context-2d canv :strokeStyle "#f00")]
+    (loop [i num-frames p p]
+      (if (pos? i)
+        (let [p (phys/update p)]
+          ;;(.log js/console "-----")
+          ;;(viz/linestrip ctx (map :pos (:particles p)))
+          (recur (dec i) p))
+        (do
+          (viz/linestrip ctx (map :pos (:particles p)))
+          (clj->js p))))))
--- src-cljs/thi/ng/physics/core.cljs
+++ src-cljs/thi/ng/physics/core.cljs
@@ -1,6 +1,8 @@
 (ns thi.ng.physics.core
   (:require
-   [thi.ng.geom.core :as g]))
+   [thi.ng.data.core :as d]
+   [thi.ng.geom.core :as g]
+   ))
 
 (defprotocol IUpdate
   (update [this]))
@@ -23,26 +25,32 @@
   (apply-force [this])
   (clear-force [this])
   (clear-velocity [this])
+  (scale-velocity [this v])
   (set-weight [this w])
   (lock [this])
   (unlock [this])
   (locked? [this]))
 
-(defprotocol IPhysics)
+(defprotocol IPhysics
+  (update-particles [this])
+  (update-springs [this]))
+
+(defprotocol ISpringUpdate
+  (update-spring [this particles]))
 
 (defrecord VerletParticle [pos prev locked? weight inv-weight force behaviors constraints]
   IBehavioral
-  (add-behavior [this b] (assoc this :behaviors (conj behaviors b)))
-  (apply-behaviors [this] (reduce #(%2 %) this behaviors))
+  (add-behavior [this b] (assoc this :behaviors (conj (:behaviors this) b)))
+  (apply-behaviors [this] (reduce #(%2 %) this (:behaviors this)))
   IConstrained
-  (add-constraint [this c] (assoc this :constraints (conj constraints c)))
-  (apply-constraints [this] (reduce #(%2 %) this constraints))
+  (add-constraint [this c] (assoc this :constraints (conj (:constraints this) c)))
+  (apply-constraints [this] (reduce #(%2 %) this (:constraints this)))
   IParticle
   (lock [this] (assoc this :locked? true))
   (unlock [this] (assoc this :locked? false))
-  (locked? [this] locked?)
+  (locked? [this] (:locked? this))
   (set-weight [this w] (assoc this :weight w :inv-weight (/ 1.0 w)))
-  (apply-force [this]
+  (apply-force [{:keys [pos prev weight force] :as this}]
     (assoc this
       :pos (-> force
                (g/scale2 weight)
@@ -50,9 +58,11 @@
                (g/add2 pos))
       :prev pos
       :force [0.0 0.0]))
+  (scale-velocity [this s]
+    (assoc this :prev (g/mix2 (:pos this) (:prev this) s)))
   IUpdate
   (update [this]
-    (if locked?
+    (if (:locked? this)
       this
       (-> this
           apply-behaviors
@@ -61,25 +71,48 @@
 
 (defrecord VerletPhysics [particles springs behaviors constraints timestep iter drag]
   IBehavioral
-  (add-behavior [this b] (assoc this :behaviors (conj behaviors b)))
+  (add-behavior [this b] (assoc this :behaviors (conj (:behaviors this) b)))
   (apply-behaviors [this]
-    (assoc this :particles (vec (d/apply-fns behaviors particles))))
+    (assoc this :particles (vec (d/apply-fns (:behaviors this) (:particles this)))))
   IConstrained
-  (add-constraint [this c] (assoc this :constraints (conj constraints c)))
+  (add-constraint [this c] (assoc this :constraints (conj (:constraints this) c)))
   (apply-constraints [this]
-    (assoc this :particles (vec (d/apply-fns constraints particles))))
+    (assoc this :particles (vec (d/apply-fns (:constraints this) (:particles this)))))
   IPhysics
-  (update-particles [this]
+  (update-particles [{:keys [drag particles] :as this}]
     (assoc this
       :particles (vec (map #(update (scale-velocity % drag)) particles))))
-  (update-springs [this]
+  (update-springs [{:keys [iter particles springs] :as this}]
     (assoc this
-      :springs (vec (reduce (fn [springs _] (map update springs)) (range iter)))))
+      :particles
+      (loop [i iter particles particles]
+        (if (pos? i)
+          (recur (dec i) (reduce (fn [particles s] (update-spring s particles)) particles springs))
+          particles))))
   IUpdate
   (update [this]
     (-> this
         apply-behaviors
         update-particles
         update-springs
-        apply-constraints))
-  )
+        apply-constraints)))
+
+(defrecord VerletSpring [aid bid rlen strength a-locked? b-locked?]
+  ISpringUpdate
+  (update-spring [{:keys [aid bid] :as this} particles]
+    ;; (.log js/console "sup" (clj->js (map :pos particles)))
+    (let [a (particles aid)
+          b (particles bid)
+          aw (:inv-weight a)
+          bw (:inv-weight b)
+          delta (g/sub2 (:pos b) (:pos a))
+          dist (+ (g/mag2 delta) 1e-6)
+          nd (* (/ (- dist (:rlen this)) (* dist (+ aw bw))) (:strength this))
+          ;;_ (.log js/console (clj->js [a b delta dist nd]))
+          particles (if (or (:locked? a) (:a-locked? this))
+                      particles
+                      (update-in particles [aid :pos] #(g/fma2 delta (* nd aw) %)))
+          particles (if (or (:locked? b) (:b-locked? this))
+                      particles
+                      (update-in particles [bid :pos] #(g/fma2 delta (* (- nd) bw) %)))]
+      particles)))
