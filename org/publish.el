312dc3542fb603b072afba4c88c01d07c8fe645e
Karsten Schmidt <k@postspectacular.com>
2013-09-09 00:43:47 +0100

    updating publish.el to use relative dir paths

--- org/publish.el
+++ org/publish.el
@@ -1,17 +1,21 @@
 (require 'org-publish)
-(setq org-publish-project-alist
-      '(("org-notes"
-         :base-directory "~/Documents/workspace.clj/thing-geom/org/"
-         :base-extension "org"
-         :publishing-directory "~/Documents/workspace.clj/thing-geom/org/html/"
-         :recursive f
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :auto-preamble f)
-        ("org-static"
-         :base-directory "~/Documents/workspace.clj/thing-geom/org/assets/"
-         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/Documents/workspace.clj/thing-geom/org/html/"
-         :recursive t
-         :publishing-function org-publish-attachment)
-        ("all" :components ("org-notes" "org-static"))))
+(let (base-dir pub-dir)
+  (setq base-dir (file-name-directory (buffer-file-name)))
+  (setq pub-dir (format "%shtml/" base-dir))
+  (setq org-publish-project-alist
+        (list
+         (list "org-notes"
+               :base-directory base-dir
+               :base-extension "org"
+               :publishing-directory pub-dir
+               :recursive 'f
+               :publishing-function 'org-html-publish-to-html
+               :headline-levels 4
+               :auto-preamble 'f)
+         (list "org-static"
+               :base-directory (format "%sassets/" base-dir)
+               :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+               :publishing-directory pub-dir
+               :recursive 't
+               :publishing-function 'org-publish-attachment)
+         '("all" :components ("org-notes" "org-static")))))
