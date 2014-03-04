12bb790e711297003a87fb1c99a6a71ee1688926
Karsten Schmidt <k@postspectacular.com>
2014-03-04 01:44:59 +0000

    add tangle flag to avoid confirmation of codeblock evals

--- tangle.sh
+++ tangle.sh
@@ -12,6 +12,7 @@ emacs -Q --batch \
     --eval \
     "(progn
      (require 'org)(require 'ob)(require 'ob-tangle)
+     (setq org-confirm-babel-evaluate nil)
      (mapc (lambda (file)
             (find-file (expand-file-name file \"$DIR\"))
             (org-babel-tangle)
