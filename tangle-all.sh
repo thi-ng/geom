98af39e451cb7f6da370784b8161df2b0d0db172
Karsten Schmidt <k@postspectacular.com>
2014-05-19 12:06:32 +0100

    add test folders to tangle-all.sh

--- tangle-all.sh
+++ tangle-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./tangle.sh \
   src/*.org \
-  geom-core/src/*.org \
-  geom-meshops/src/*.org \
-  geom-types/src/*.org
+  geom-core/src/*.org geom-core/test/*.org \
+  geom-meshops/src/*.org geom-meshops/test/*.org \
+  geom-types/src/*.org geom-types/test/*.org
