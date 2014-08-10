02b00429aa745b7c7fe3c22a2507a6d1a0b6fc0d
Karsten Schmidt <k@postspectacular.com>
2014-08-10 12:59:56 +0100

    update build scripts

--- deploy-all.sh
+++ deploy-all.sh
@@ -6,7 +6,7 @@ readonly ARGS="$@"
 readonly PREFIX="geom-"
 
 main() {
-    local modules="core types meshops webgl"
+    local modules="core types meshops webgl svg"
 
     for m in $modules
     do
--- tangle-all.sh
+++ tangle-all.sh
@@ -2,5 +2,7 @@
 ./tangle.sh \
   src/*.org \
   geom-core/src/*.org geom-core/test/*.org \
+  geom-types/src/*.org geom-types/test/*.org \
   geom-meshops/src/*.org geom-meshops/test/*.org \
-  geom-types/src/*.org geom-types/test/*.org
+  geom-webgl/src/*.org geom-webgl/test/*.org \
+  geom-svg/src/*.org geom-svg/test/*.org
