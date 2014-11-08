399199b37aa405f125f9d1c61a5ba9d02a986a6f
Karsten Schmidt <k@postspectacular.com>
2014-11-08 19:43:52 +0000

    add test-all.sh

---
+++ test-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(cd "$(dirname "$0")"; pwd)
+readonly ARGS="$@"
+readonly PREFIX="geom-"
+
+main() {
+    local modules="core types meshops physics svg webgl"
+
+    for m in $modules
+    do
+        echo "testing module $PREFIX$m..."
+        echo "----------------------------------------"
+        cd $PROGDIR/$PREFIX$m/babel
+        lein do clean, cljx once
+        lein test
+        lein cljsbuild test
+    done
+}
+
+main
