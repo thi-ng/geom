bbdc01ae8d6e6e01b66ae75dc19e2780b8f20a08
Karsten Schmidt <k@postspectacular.com>
2015-02-22 18:00:05 +0000

    [all] update thi.ng/color dep, add clean step to install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -13,11 +13,14 @@ main() {
         echo "installing module $PREFIX$m..."
         echo "----------------------------------------"
         cd $PROGDIR/$PREFIX$m/babel
-        lein do cljx once, install
+        lein do clean, cljx once, install
     done
 
     cd $PROGDIR/geom-webgl/babel
-    lein install
+    lein do clean, install
+
+    cd $PROGDIR/babel
+    lein do clean, install
 }
 
 main
