9310f84d0b913fcbb36e2b69dcca811836dc62b9
Karsten Schmidt <k@postspectacular.com>
2015-03-15 21:39:17 +0000

    add install-module.sh

---
+++ install-module.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(cd "$(dirname "$0")"; pwd)
+readonly ARGS="$@"
+readonly PREFIX="geom-"
+
+main() {
+    for m in $ARGS
+    do
+        echo "installing module $PREFIX$m..."
+        echo "----------------------------------------"
+        cd $PROGDIR/$PREFIX$m/babel
+        lein do clean, cljx once, install
+    done
+}
+
+main
