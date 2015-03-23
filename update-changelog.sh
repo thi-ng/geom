5a884ee79743b09fad4e3a0d321640e9d140de52
Karsten Schmidt <k@postspectacular.com>
2015-03-23 16:47:29 +0000

    [meta] update changelog & script (add dates)

--- update-changelog.sh
+++ update-changelog.sh
@@ -1,4 +1,4 @@
-echo -e "** $2\n" > CHANGELOG.$$
+echo -e "** $2 - `date +%Y-%m-%d`\n" > CHANGELOG.$$
 git log $1...$2 --pretty=format:'| [[https://github.com/thi-ng/geom/commit/%H][%h]] | %s |' >> CHANGELOG.$$
 echo -e "\n" >> CHANGELOG.$$
 cat CHANGELOG.org >> CHANGELOG.$$
