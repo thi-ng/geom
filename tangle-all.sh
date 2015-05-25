3ba44eaa9b11e337b4d21ac0960d9ff3f3da9909
Karsten Schmidt <k@postspectacular.com>
2015-05-25 23:23:58 +0100

    [meta] update tangle-module & tangle-all to clear babel/src & babel/test dirs

--- tangle-all.sh
+++ tangle-all.sh
@@ -6,6 +6,7 @@ FILES="src/index.org"
 for m in $MODULES; do
     MODULE="geom-$m"
     SRC="$MODULE/src/*.org $MODULE/test/*.org $MODULE/bench/*.org"
+    rm -rf $MODULE/babel/src $MODULE/babel/test
     for f in `ls $SRC`; do
         FILES="$FILES $f"
     done
--- tangle-module.sh
+++ tangle-module.sh
@@ -4,6 +4,8 @@ DIR=`pwd`
 FILES=""
 SRC="geom-$1/src/*.org geom-$1/test/*.org geom-$1/bench/*.org"
 
+rm -rf geom-$1/babel/src geom-$1/babel/test
+
 # wrap each argument in the code required to call tangle on it
 for i in `ls $SRC`; do
     FILES="$FILES \"$i\""
