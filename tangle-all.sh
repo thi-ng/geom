3acf1e0ba81b8d997d3c23c5ded51995af782351
Karsten Schmidt <k@postspectacular.com>
2016-03-16 14:26:51 +0000

    [all] start project restructure
    - removing individual modules in favor of single build for easier dev & usage
    - check in migrated core namespaces (shared /src, /test, /bench & /examples dirs)
    - use main index.org as main README.org

--- tangle-all.sh
+++ tangle-all.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
+./tangle.sh README.org
+
 readonly MODULES="core types meshops physics svg viz voxel webgl"
-FILES="src/index.org"
 
 for m in $MODULES; do
-    MODULE="geom-$m"
-    SRC="$MODULE/src/*.org $MODULE/test/*.org $MODULE/bench/*.org"
-    rm -rf $MODULE/babel/src $MODULE/babel/test
-    for f in `ls $SRC`; do
-        FILES="$FILES $f"
-    done
+    ./tangle-module.sh $m
 done
-
-#echo $FILES
-./tangle.sh $FILES
--- tangle-module.sh
+++ tangle-module.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 
-DIR=`pwd`
 FILES=""
-SRC="geom-$1/src/*.org geom-$1/test/*.org geom-$1/bench/*.org"
+readonly SRC="src/$1/*.org test/$1/*.org bench/$1/*.org"
 
-rm -rf geom-$1/babel/src geom-$1/babel/test
+rm -rf babel/src/thi/ng/geom/$1 babel/test/thi/ng/geom/test/$1
 
-# wrap each argument in the code required to call tangle on it
-for i in `ls $SRC`; do
-    FILES="$FILES \"$i\""
+for f in `ls $SRC`; do
+    FILES="$FILES $f"
 done
 
-emacs -Q --batch \
-    --eval \
-    "(progn
-     (require 'org)(require 'ob)(require 'ob-tangle)(require 'ob-lob)
-     (org-babel-lob-ingest \"src/library-of-babel.org\")
-     (org-babel-lob-ingest \"src/config.org\")
-     (setq org-confirm-babel-evaluate nil)
-     (mapc (lambda (file)
-            (find-file (expand-file-name file \"$DIR\"))
-            (org-babel-tangle)
-            (kill-buffer)) '($FILES)))" \
-#2>&1 | grep Tangled
+./tangle.sh $FILES
