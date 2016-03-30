eed00cbfc7fe50c3d6c04f7fe86ae4221956fb8e
Karsten Schmidt <k@postspectacular.com>
2016-03-30 17:43:38 +0100

    [gl] rename geom.webgl namespaces to geom.gl.*
    - update references
    - add JOGL constants (OpenGL, all versions)
    - update tangle-all.sh

--- tangle-all.sh
+++ tangle-all.sh
@@ -2,7 +2,7 @@
 
 ./tangle.sh README.org
 
-readonly MODULES="core mesh physics svg types utils viz voxel webgl"
+readonly MODULES="core mesh physics svg types utils viz voxel gl"
 
 for m in $MODULES; do
     ./tangle-module.sh $m
